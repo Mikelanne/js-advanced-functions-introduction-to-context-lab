d0f74c57e55dfe44121934bcddd0e2ea8141e93e
Mikelanne <mikelanne.wood@gmail.com>
2021-04-11 16:59:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function createEmployeeRecord(array) {
+    const employee = {
+        firstName: (array[0]),
+        familyName: (array[1]),
+        title: (array[2]),
+        payPerHour: (array[3])
+    }
+}
